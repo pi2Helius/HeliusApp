ef8cf580acb7205b2167910fd2c34cb45d0608ef
lugope <lucasgpereira.10@gmail.com>
2019-11-27 17:46:49 -0300

    Getting solar radiation from Firebase to Weather screen.

--- helius_app/lib/pages/charts_weather_page.dart
+++ helius_app/lib/pages/charts_weather_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:helius_app/components/chart_card.dart';
 import 'package:http/http.dart' as http;
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class WeatherScreen extends StatefulWidget {
   @override
@@ -12,8 +13,9 @@ class WeatherScreen extends StatefulWidget {
 
 class _WeatherScreenPage extends State<WeatherScreen> {
   //Variables
-  Map<String, dynamic> weatherData = Map<String, dynamic>();
+  String radiation = "";
   var isLoading = true;
+  Map<String, dynamic> weatherData = Map<String, dynamic>();
   final String weatherURL = "http://apiadvisor.climatempo.com.br/api/v1/weather/locale/8173/current?token=6aece1fb4baacadf9d93355fe9a39068";
   
   ChartCard cardMaker = ChartCard();
@@ -102,7 +104,7 @@ class _WeatherScreenPage extends State<WeatherScreen> {
       ),
       Padding(
         padding: const EdgeInsets.all(4.0),
-        child: cardMaker.card(_cardContent("Irradiação Solar", "-", "W/m²")),
+        child: cardMaker.card(_cardContent("Irradiação Solar", radiation, "W/m²")),
       ),
     ];
   }
@@ -137,13 +139,23 @@ class _WeatherScreenPage extends State<WeatherScreen> {
       isLoading = true;
     });
 
+    // Getting data from Firebase
+    CollectionReference heliusCollection = Firestore.instance.collection('Usina');
+    DocumentReference documentReference = heliusCollection.document('teste');
+    DocumentSnapshot documentSnapshot = await documentReference.get();
+    setState(() {
+      radiation = documentSnapshot['IRRAD_LDR'].round().toString();
+    });
+
+    // Getting data from API
     final response = await http.get(weatherURL);
-    print('Waiting response...');
+    // print('Waiting response...');
 
     if (response.statusCode == 200){
-      print('Got response!');
+      // print('Got response!');
       weatherData = json.decode(response.body);
-      print('Response with id ' + weatherData['id'].toString());
+      // print('Response with id ' + weatherData['id'].toString());
+
       setState(() {
         isLoading = false;
       });
